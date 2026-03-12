024e9422f9621ff11ee8cd7ae152bf2a8caf860c
Kaim <alexwkaim22@hotmail.com>
2026-03-11 18:21:43 -0700

    Fix scaling labels: primary=scaling in, split=scaling out always

--- monitor.py
+++ monitor.py
@@ -110,8 +110,9 @@ def build_message(tier_name, sz, split_name, split_sz, btc_price, quantile):
         t_qmin = next(t["qMin"] for t in LADDER if t["name"] == tier_name)
         s_qmin = next(t["qMin"] for t in LADDER if t["name"] == split_name)
         split_is_riskier = s_qmin < t_qmin
-        pos.append(f"{tier_name}  <b>{sz}%</b>{'  <i>scaling in</i>' if split_is_riskier else '  <i>scaling out</i>'}")
-        pos.append(f"{split_name}  <b>{split_sz}%</b>{'  <i>scaling out</i>' if split_is_riskier else '  <i>scaling in</i>'}")
+        # Primary (larger %) is always scaling in; split (smaller %) is always scaling out
+        pos.append(f"{tier_name}  <b>{sz}%</b>  <i>scaling in</i>")
+        pos.append(f"{split_name}  <b>{split_sz}%</b>  <i>scaling out</i>")
     else:
         pos.append(f"{tier_name}  <b>{sz}%</b>")
     parts.append("\n".join(pos))
